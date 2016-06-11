ae50a694b7942b6ba83784a03f69930e9efd3b48
vesselintodorov <vesselintodorov@mail.bg>
2016-06-12 00:18:43 +0300

    Added extensions for identity user for using in views.

--- Source/Data/EventSystem.Data.Models/ApplicationUser.cs
+++ Source/Data/EventSystem.Data.Models/ApplicationUser.cs
@@ -23,6 +23,10 @@
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
+            userIdentity.AddClaim(new Claim("FirstName", this.FirstName));
+            userIdentity.AddClaim(new Claim("LastName", this.LastName));
+
+
             return userIdentity;
         }
 
---
+++ Source/Web/EventSystem.Web/Extensions/IdentityExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace EventSystem.Web.Extensions
+{
+    using System.Security.Claims;
+    using System.Security.Principal;
+
+    public static class IdentityExtensions
+    {
+        public static string GetFirstName(this IIdentity identity)
+        {
+            var claim = ((ClaimsIdentity)identity).FindFirst("FirstName");
+            return (claim != null) ? claim.Value : string.Empty;
+        }
+
+        public static string GetLastName(this IIdentity identity)
+        {
+            var claim = ((ClaimsIdentity)identity).FindFirst("LastName");
+            return (claim != null) ? claim.Value : string.Empty;
+        }
+    }
+}
